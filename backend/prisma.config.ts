80c10b3ebf74b250c12c79e033c39e35225582d4
Abdulrhman-Alshafi <Abdulrahman.Alshafi@optimumpartners.com>
2025-11-25 10:22:52 +0300

    adding the schema

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,12 +1,15 @@
+import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma/schema.prisma", // path to your schema
   migrations: {
     path: "prisma/migrations",
   },
-  engine: "classic",
   datasource: {
     url: env("DATABASE_URL"),
+    // If you use a shadow database:
+    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
   },
+  // other config options ...
 });
